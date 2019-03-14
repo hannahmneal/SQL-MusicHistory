8ccc4c19f50bdd7f56045687d7aaff8985b3ebf9
Hannah Neal <hannahmneal@gmail.com>
2019-03-14 15:19:18 -0700

    added gitignore

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MusicHistory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
